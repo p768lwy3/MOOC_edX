951f6437c0292d57c3764fb25d8a56570395781a
LI, Wai Yin <p768lwy3@gmail.com>
2018-01-03 21:42:59 +0800

    Create problem3.py

---
+++ KyotoUx009x - Stochastic Processes - Data Analysis and Computer Simulation/Week 4/problem3.py
@@ -0,0 +1,37 @@
+import numpy as np # import numpy library as np
+import matplotlib.pyplot as plt # import pyplot library as plt 
+from mpl_toolkits.mplot3d import Axes3D # import Axes3D from `mpl_toolkits.mplot3d`
+plt.style.use('ggplot') # use "ggplot" style for graphs
+dim  = 3 # system dimension (x,y,z)
+nump = 100 # number of independent Brownian particles to simulate 
+nums = 1024 # number of simulation steps
+dt   = 0.05 # set time increment, \Delta t
+zeta = 0.5 # set friction constant, \zeta
+m    = 1.0 # set particle mass, m
+kBT  = 0.5 # set temperatute, k_B T
+std  = np.sqrt(2*kBT*zeta*dt) # calculate std for \Delta W via Eq.(F11)
+np.random.seed(0) # initialize random number generator with a seed=0
+R = np.zeros([nump,dim]) # array to store current positions and set initial condition Eq.(F12)
+V = np.zeros([nump,dim]) # array to store current velocities and set initial condition Eq.(F12)
+W = np.zeros([nump,dim]) # array to store current random forcces
+Rs = np.zeros([nums,nump,dim]) # array to store positions at all steps
+Vs = np.zeros([nums,nump,dim]) # array to store velocities at all steps
+Ws = np.zeros([nums,nump,dim]) # array to store random forces at all steps
+time = np.zeros([nums]) # an array to store time at all steps
+for i in range(nums): # repeat the following operations from i=0 to nums-1
+    W = std*np.random.randn(nump,dim) # generate an array of random forces accordingly to Eqs.(F10) and (F11)
+    V = V*(1-zeta/m*dt)+W/m # update velocity via Eq.(F9)
+    R = R + V*dt # update position via Eq.(F5)
+    Rs[i,:,:]=R # accumulate particle positions at each step in an array Rs
+    Vs[i,:,:]=V # accumulate particle velocitys at each step in an array Vs
+    Ws[i,:,:]=W # accumulate random forces at each step in an array Ws
+    time[i]=i*dt # store time in each step in an array time
+box=80. # set draw area as box^2
+fig, ax = plt.subplots(figsize=(7.5,7.5)) # set fig with its size 7.5 x 7.5 inch
+ax.set_xlabel(r"$R_x$", fontsize=20) # set x-label
+ax.set_ylabel(r"$R_z$", fontsize=20) # set y-label
+plt.xlim(-box/2,box/2) # set x-range
+plt.ylim(-box/2,box/2) # set y-range
+for n in range(nump): # repeat from n=0 to nump-1
+    ax.plot(Rs[:,n,0],Rs[:,n,2],alpha=0.5) # plot trajectiries of all particles
+plt.show() # draw plots
